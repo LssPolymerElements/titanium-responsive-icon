b29e2c4ea090fadc29e03c4e9bec58ec360f47aa
Unknown <anthony-thomas@leavitt.com>
2018-06-04 11:35:16 -0600

    Applies clang formatting. Adds reflect metadata

--- src/titanium-responsive-icon.ts
+++ src/titanium-responsive-icon.ts
@@ -1,37 +1,29 @@
-@Polymer.decorators.customElement('titanium-responsive-icon')
-class TitaniumResponsiveIcon extends Polymer.Element {
+@Polymer
+    .decorators.customElement('titanium-responsive-icon') class TitaniumResponsiveIcon extends Polymer.Element {
+  @Polymer.decorators.property() icon: string;
 
-    @Polymer.decorators.property()
-    icon: string;
+  @Polymer.decorators.property() focused: boolean = false;
 
-    @Polymer.decorators.property()
-    focused: boolean = false;
+  @Polymer.decorators.property() invalid: boolean = false;
 
-    @Polymer.decorators.property()
-    invalid: boolean = false;
+  @Polymer.decorators.property() attrForFocused: string = 'focused';
 
-    @Polymer.decorators.property()
-    attrForFocused: string = 'focused';
+  @Polymer.decorators.property() attrForInvalid: string = 'invalid';
 
-    @Polymer.decorators.property()
-    attrForInvalid: string = 'invalid';
+  ready() {
+    super.ready();
 
-    ready() {
-        super.ready();
+    if (!this.parentElement)
+      return;
 
-        if (!this.parentElement)
-            return;
+    this.parentElement.addEventListener(`${this.attrForInvalid}-changed`, (e: any) => {
+      if (e.detail)
+        this.invalid = e.detail.value;
+    });
 
-        this.parentElement.addEventListener(`${this.attrForInvalid}-changed`, (e: any) => {
-            if (e.detail)
-                this.invalid = e.detail.value;
-        });
-
-        this.parentElement.addEventListener(`${this.attrForFocused}-changed`, (e: any) => {
-            if (e.detail)
-                this.focused = e.detail.value;
-        });
-
-
-    }
+    this.parentElement.addEventListener(`${this.attrForFocused}-changed`, (e: any) => {
+      if (e.detail)
+        this.focused = e.detail.value;
+    });
+  }
 }
